fae81a456495fc124b81464beaf5dc4301f345fe
Sai Chandramouli <schand31@asu.edu>
2017-01-01 01:34:10 -0800

    finished components section, moving to events

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ class ExampleComponent extends Component {
           <li>Properties are set on a component just like html attributes, i.e. {'<tagname attrName="attrValue"></tagname>'}</li>
           <li>Properties can be accessed inside a component by using the props object.</li>
           <li>Only requirement for a component is that it should implement the render method (makes sense, otherwise how will it display?)</li>
+          <li>(Also a pseudo requirement seems to be they need to be capitalized, i.e. Comp vs comp.)</li>
           <li>`this` keyword usage needs to be understood (usual JS)</li>
+          <li>Class style doesnt support mixins</li>
         </ul>
         <p>Components can also have state, accessed similar to props: this.state</p>
         <h4>This is my state: {JSON.stringify(this.state)}</h4>
@@ -30,8 +32,8 @@ class ExampleComponent extends Component {
   }
 }
 
-let AnotherComponent = React.createClass({
-  getInitialState () {
+const AnotherComponent = React.createClass({
+  getInitialState: function () {
     return {
       name: this.props.name,
       message: this.props.message
@@ -43,11 +45,12 @@ let AnotherComponent = React.createClass({
         <h3>{this.props.name}</h3>
         <p>My message is : {this.props.message} </p>
         <ul>
-          <li>This is also a component, you can either use the ES6 class style () as before, or the regular JS object literal style.</li>
+          <li>This is also a component, you can either use the ES6 class style as before, or the regular JS object literal style.</li>
           <li>Lets try similar functionality as before</li>
           <li>The object literal style though, is react specific</li>
           <li>It does not require a constructor, instead, if you want to set default state, use getInitialState</li>
           <li>Also, more importantly, `this` keyword binding is done automatically if you use the createClass()</li>
+          <li>Object literal supports mixins</li>
         </ul>
         <h4>This is my state: {JSON.stringify(this.state)}</h4>
       </div>
