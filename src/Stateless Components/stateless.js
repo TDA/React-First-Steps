9386a555bad5b539bd6f51b2f3aea33dd273aa5c
Sai Chandramouli <schand31@asu.edu>
2018-02-07 23:54:34 -0800

    starting stateless

--- src/Stateless Components/stateless.js
+++ src/Stateless Components/stateless.js
@@ -1,5 +1,16 @@
+import React from 'react';
+
 // Use the function syntax for stateless components (not the Class style, and
 // don't confuse this with the React.createClass style.
 const HelloWorld = function (props) {
   return <h1 {...props}>Hello {props.name}!!!</h1>
+};
+
+// Or even more concise:
+const HelloWorldConcise = (props) => { <h1 {...props}>Hello {props.name}!!!</h1> };
+
+
+module.exports = {
+  HelloWorld: HelloWorld,
+  HelloWorldConcise: HelloWorldConcise
 };
