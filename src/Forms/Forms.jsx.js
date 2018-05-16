10d3957b37650bfcf5871cf2b86192f6e1e933e3
Sai Chandramouli <schand31@asu.edu>
2018-05-15 23:00:20 -0700

    adding radio buttons

--- src/Forms/Forms.jsx.js
+++ src/Forms/Forms.jsx.js
@@ -7,24 +7,47 @@ class StaticForm extends React.Component {
 }
 
 class DynamicForm extends React.Component {
-
   constructor(props, context) {
     super(props, context);
     this.state = {
-      title: ""
+      title: "",
+      jsStuff: {
+        angular: false,
+        react: false,
+        polymer: false
+      }
     };
+    this.handleRadio = this.handleRadio.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
   }
 
+  handleRadio(event) {
+    let obj = {};  // erase other radios
+    obj[event.target.value] = event.target.checked;
 
-
-  render() {
-    return <input type="text" name="title" value={this.state.title} onChange={this.handleTitleChange.bind(this)} />
+    this.setState({
+      jsStuff: obj
+    })
   }
 
   handleTitleChange(event) {
     this.setState({
       title: event.target.value
     })
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="title">Title:</label><input type="text" name="title" value={this.state.title} onChange={this.handleTitleChange} />
+        <br/>
+        <radiogroup>
+          <input type="radio" name="jsStuff" onChange={this.handleRadio} value='angular' id='angular' checked={this.state.jsStuff['angular']}/><label htmlFor='angular'>angular</label><br/>
+          <input type="radio" name="jsStuff" onChange={this.handleRadio} value='react' id='react' checked={this.state.jsStuff['react']}/><label htmlFor='react'>react</label><br/>
+          <input type="radio" name="jsStuff" onChange={this.handleRadio} value='vue' id='vue' checked={this.state.jsStuff['vue']}/><label htmlFor='vue'>vue</label>
+        </radiogroup>
+      </form>
+    )
   }
 }
 
