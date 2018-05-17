7047f7943972baa2b8002955f5d0991cc33ea637
Sai Chandramouli <schand31@asu.edu>
2018-05-16 23:02:54 -0700

    adding checkboxes

--- src/Forms/Forms.jsx.js
+++ src/Forms/Forms.jsx.js
@@ -15,6 +15,11 @@ class DynamicForm extends React.Component {
         angular: false,
         react: false,
         polymer: false
+      },
+      cssStuff: {
+        css1: false,
+        css2: false,
+        css3: false
       }
     };
     this.handleRadio = this.handleRadio.bind(this);
@@ -27,6 +32,15 @@ class DynamicForm extends React.Component {
 
     this.setState({
       jsStuff: obj
+    })
+  }
+
+  handleCheckbox(event) {
+    let obj = {};  // erase other radios
+    obj[event.target.value] = event.target.checked;
+
+    this.setState({
+      cssStuff: obj
     })
   }
 
@@ -46,6 +60,11 @@ class DynamicForm extends React.Component {
           <input type="radio" name="jsStuff" onChange={this.handleRadio} value='react' id='react' checked={this.state.jsStuff['react']}/><label htmlFor='react'>react</label><br/>
           <input type="radio" name="jsStuff" onChange={this.handleRadio} value='vue' id='vue' checked={this.state.jsStuff['vue']}/><label htmlFor='vue'>vue</label>
         </radiogroup>
+        <checkboxgroup>
+          <input type="checkbox" name="cssStuff" onChange={this.handleCheckbox} value='css1' id='css1' checked={this.state.cssStuff['css1']}/><label htmlFor='css1'>css1</label><br/>
+          <input type="checkbox" name="cssStuff" onChange={this.handleCheckbox} value='css2' id='css2' checked={this.state.cssStuff['css2']}/><label htmlFor='css2'>css2</label><br/>
+          <input type="checkbox" name="cssStuff" onChange={this.handleCheckbox} value='css3' id='css3' checked={this.state.cssStuff['css3']}/><label htmlFor='css3'>css3</label>
+        </checkboxgroup>
       </form>
     )
   }
